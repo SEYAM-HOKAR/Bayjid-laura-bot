d9fde2a8bb8168eb366d114c22b2ff43e2fcf1b8
BaYjid-008 <bayjidrahman24@gmail.com>
2025-10-31 12:44:29 +0600

    Update tiksr.js

--- scripts/cmds/tiksr.js
+++ scripts/cmds/tiksr.js
@@ -10,7 +10,7 @@ async function getStreamFromURL(url) {
 
 module.exports = {
   config: {
-    name: "tiktok",
+    name: "tiksr",
     aliases: ["tikvd"],
     author: "BaYjid",
     version: "1.0.3",
